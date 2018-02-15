9e4579768ab419f6d0acf9b162723a6164ddf590
CodeBrew28 <arshiam2@illinois.edu>
2018-02-14 18:38:47 -0600

    fixed webpack:

--- web_backend/webpack.config.js
+++ web_backend/webpack.config.js
@@ -9,18 +9,5 @@ module.exports = {
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js',
-  },
-  plugins: [
-    new webpack.NamedModulesPlugin(),
-    new webpack.NoEmitOnErrorsPlugin(),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.js?$/,
-        use: [ 'babel-loader' ],
-        include: path.join(__dirname, 'src'),
-      },
-    ],
-  },
+  }
 };
