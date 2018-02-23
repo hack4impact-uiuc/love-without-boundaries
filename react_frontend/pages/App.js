fd5b79783d5929b00c9d2f9daa88e9ecc8e34e72
CodeBrew28 <arshiam2@illinois.edu>
2018-02-22 21:49:24 -0600

    pages:

--- react_frontend/pages/App.js
+++ react_frontend/pages/App.js
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import { graphql, QueryRenderer } from 'react-relay';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 import environment from './../relay/environment';
 import StudentListItem from './../components/studentListItem';
@@ -9,8 +10,38 @@ import Button from './../components/button';
 import addStudent from './../relay/mutations/addStudent';
 import Lesson from './../components/lesson';
 
+const Home = () => (
+    <div>
+      <h2>Home</h2>
+    </div>
+);
+  
+const About = () => (
+    <div>
+      <h2>About</h2>
+    </div>
+  );
+  
+
 export default () => (
     <div>
+        <Router>
+            <div>
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/about">About</Link>
+                    </li>
+                </ul>
+
+                <hr />
+
+                <Route exact path="/" component={Home} />
+                <Route path="/about" component={About} />
+            </div>
+        </Router>
         <QueryRenderer
             environment={environment}
             query={graphql`
@@ -40,3 +71,4 @@ export default () => (
         />
     </div>
 );
+
