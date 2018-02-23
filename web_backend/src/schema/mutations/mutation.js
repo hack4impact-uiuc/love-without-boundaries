9cec873cb3c9d697eb0587593c6198bdd53f479b
CodeBrew28 <arshiam2@illinois.edu>
2018-02-23 01:46:56 -0600

    query works, mutation in progress:

--- web_backend/src/schema/mutations/mutation.js
+++ web_backend/src/schema/mutations/mutation.js
@@ -1,8 +1,7 @@
 import { GraphQLObjectType, GraphQLString } from 'graphql';
-
+import GradeType from '../types/GradeType'
 import StudentType from '../types/StudentType'
 import Student from '../../models/student';
-
 
 const Mutation = new GraphQLObjectType({
   name: 'Mutation',
@@ -17,7 +16,21 @@ const Mutation = new GraphQLObjectType({
           return s.save()
         } 
       },
+      addGrade: {
+        type: StudentType,
+        args: { user: { type: GraphQLString } },
+        resolve(root, { user}, ctx) {
+          // const s = Student.findOne({"user": user}).update({"grade": {"lesson": "test", "grade" : 100}})
+          // const s = new Student({ user, teacher,});
+          return Student.findOne({ "user": "aria" }, function (err, u){
+            u.grades = new Grade({"lesson": "verbs"});
+            u.save();
+          });
 
+
+        } 
+
+      }
 
 
 
