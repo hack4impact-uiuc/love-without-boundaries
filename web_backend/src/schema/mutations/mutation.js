2ba1bd4790be502f146376fad99646753d656c48
Timothy Ko <timothy.l.ko@gmail.com>
2018-02-22 23:41:37 -0600

    almost

--- web_backend/src/schema/mutations/mutation.js
+++ web_backend/src/schema/mutations/mutation.js
@@ -5,24 +5,22 @@ import GradeType from '../types/GradeType';
 
 
 const Mutation = new GraphQLObjectType({
-  name: 'Mutation',
-  description: 'Your Root Mutation',
-  fields() {
+    name: 'Mutation',
+    description: 'Your Root Mutation',
+ 
     return {
-      createStudent: {
-        type: StudentType,
-        args: { user: { type: GraphQLString }, teacher: { type: GraphQLString }, grades: {
-          type: new GraphQLList(GraphQLString)} },
-        resolve(root, { user, teacher, grades }, ctx) {
-          const s = new Student({ user, teacher, grades });
-          return s.save()
-        } 
-      },
-
-
-
-
-    };
+        createStudent: {
+            type: StudentType,
+            args: { 
+                user: { type: GraphQLString }, 
+                teacher: { type: GraphQLString }, 
+                grades: { type: new GraphQLList(GraphQLString)} 
+            },
+            resolve(root, { user, teacher, grades }, ctx) {
+                const s = new Student({ user, teacher, grades });
+                return s.save();
+            }
+        }
   },
 });
 
