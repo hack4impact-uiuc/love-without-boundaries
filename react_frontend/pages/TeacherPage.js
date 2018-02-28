a4e3b0501f9539fe94f4cdae45cfed864d4afbe7
Hana Rimawi <hrimaw2@illinois.edu>
2018-02-28 12:30:33 -0600

    teacher page now has lesson, looks just like student page

--- react_frontend/pages/TeacherPage.js
+++ react_frontend/pages/TeacherPage.js
@@ -1,15 +1,25 @@
 import * as React from 'react';
 import styled from 'styled-components';
+import Lesson from './../components/lesson';
 
 type Props = {
-    /**/ 
+    /**/
   }
 
 class TeacherPage extends React.Component<Props>{
 
     render() {
         return (
-            <div>I am a teacher</div>
+            <div>I am a teacher page
+              <Lesson lessonName = {"Lesson 1"}
+                      lessonNotes = {"Nouns"}
+                      worksheetName = {"Worksheet 1"}
+                      worksheetIsChecked = {true}
+                      quizName = {"Nouns"}
+                      quizIsChecked = {true}
+                      quizPercentage = {'98%'}
+              >This is sample lesson Nouns</Lesson>
+            </div>
         );
     }
 }
