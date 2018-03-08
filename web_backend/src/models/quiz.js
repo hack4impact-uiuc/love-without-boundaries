c24e3b6d36133d2c5b4802a29e8b5c9f4dad8cf4
Alan Ren <alanren2@illinois.edu>
2018-03-08 00:16:28 -0600

    new question schema

--- web_backend/src/models/quiz.js
+++ web_backend/src/models/quiz.js
@@ -4,7 +4,24 @@ const Schema = mongoose.Schema;
 
 const QuizSchema = new Schema({
     quizName: { type: String },
-    listOfQuestionIDs : [{ type: String }]
+    listofQuestions: [
+       question: [ 
+         questionName: String,
+           answers: {
+           answerName: String,
+           isCorrect: String
+         }
+       ]
+    ]
   });
   
 export default mongoose.model('Quiz', QuizSchema);
+
+
+// addQuestion( quizId: ID, question: { text: String, answers: [{ text: String, isCorrect: Boolean }] }) {
+//   questions [{
+
+//   }]
+// }
+
+// deleteQuestion (quizId, 0)
