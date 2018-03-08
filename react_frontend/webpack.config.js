0d4eb63efd9a79f2f52f6079281edca9c8953e08
Timothy Ko <timothy.l.ko@gmail.com>
2018-03-07 23:55:50 -0600

    webpack styling fixes

--- react_frontend/webpack.config.js
+++ react_frontend/webpack.config.js
@@ -1,30 +1,30 @@
-var webpack = require('webpack');
-var path = require('path');
+let webpack = require('webpack');
+const path = require('path');
 
-var parentDir = path.join(__dirname, '');
+const parentDir = path.join(__dirname, '');
 
 module.exports = {
     entry: [
-        path.join(parentDir, 'index.js')
+        path.join(parentDir, 'index.js'),
     ],
     module: {
         loaders: [{
             test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader'
-            },{
-                test: /\.less$/,
-                loaders: ["style-loader", "css-loader", "less-loader"]
-            }
-        ]
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+        }, {
+            test: /\.less$/,
+            loaders: ['style-loader', 'css-loader', 'less-loader'],
+        },
+        ],
     },
     output: {
-        path: parentDir + './dist',
-        filename: 'bundle.js'
+        path: `${parentDir  }./dist`,
+        filename: 'bundle.js',
     },
     devtool: 'source-map',
     devServer: {
         contentBase: parentDir,
-        historyApiFallback: true
-    }
-}
+        historyApiFallback: true,
+    },
+};
