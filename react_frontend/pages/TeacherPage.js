e6874f1201cc88eda0627acd562399bca1629404
shreyshrey1 <talk2shreyas@gmail.com>
2018-03-10 14:06:34 -0600

    small fixes

--- react_frontend/pages/TeacherPage.js
+++ react_frontend/pages/TeacherPage.js
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import AddLesson from './../components/addlesson';
 import Lesson from './../components/lesson';
