244ef39c02b25fe6cb9232f77928ee837f3584dc
Timothy Ko <timothy.l.ko@gmail.com>
2018-03-12 15:57:54 -0500

    styling

--- react_frontend/src/components/login.js
+++ react_frontend/src/components/login.js
@@ -9,6 +9,19 @@ class Login extends React.Component{
     render(){
         return(
             <GoogleLogin
+                className="btn"
+                style={{
+                    backgroundColor: "#C04448", 
+                    color: "#ffffff",
+                    height: "50px",
+                    width: "100px",
+                    padding: "12px 20px",
+                    margin: "10px 10px",
+                    textAlign: "center",
+                    borderRadius: "6px",
+                    fontSize: "16px",
+                    verticalAlign: "middle"
+                }}
                 clientId="162938498619-oloa040ksgc64aubtv7hi7pmnbanmmul.apps.googleusercontent.com"
                 buttonText="Login"
                 onSuccess={this.responseGoogle}
