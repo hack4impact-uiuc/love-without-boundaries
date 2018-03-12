6b1810a17bcfc30c711d88e62084d230ef323c6f
Timothy Ko <timothy.l.ko@gmail.com>
2018-03-12 17:35:18 -0500

    login redirects to student page if successful

--- react_frontend/src/components/login.js
+++ react_frontend/src/components/login.js
@@ -1,33 +1,38 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom'
 import { GoogleLogin } from 'react-google-login';
 
 
-class Login extends React.Component{
+class Login extends React.Component {
     responseGoogle = (response) => {
+        console.log(response);
+        this.props.history.push('/student');
+    }
+    failedGoogle = (response) =>{
         console.log(response);
     }
     render(){
         return(
             <GoogleLogin
-                className="btn"
+                className='btn'
                 style={{
-                    backgroundColor: "#C04448", 
-                    color: "#ffffff",
-                    height: "50px",
-                    width: "100px",
-                    padding: "12px 20px",
-                    margin: "10px 10px",
-                    textAlign: "center",
-                    borderRadius: "6px",
-                    fontSize: "16px",
-                    verticalAlign: "middle"
+                    backgroundColor: '#C04448', 
+                    color: '#ffffff',
+                    height: '50px',
+                    width: '100px',
+                    padding: '12px 20px',
+                    margin: '10px 10px',
+                    textAlign: 'center',
+                    borderRadius: '6px',
+                    fontSize: '16px',
+                    verticalAlign: 'middle'
                 }}
-                clientId="162938498619-oloa040ksgc64aubtv7hi7pmnbanmmul.apps.googleusercontent.com"
-                buttonText="Login"
+                clientId='162938498619-oloa040ksgc64aubtv7hi7pmnbanmmul.apps.googleusercontent.com'
+                buttonText='Login'
                 onSuccess={this.responseGoogle}
-                onFailure={this.responseGoogle}
+                onFailure={this.failedGoogle}
             />
         );
     }
 }
-export default Login;
+export default withRouter(Login);
