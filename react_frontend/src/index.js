13efce5d3b173d986fd2245d45dd1faccb5c15f8
Timothy Ko <timothy.l.ko@gmail.com>
2018-03-16 12:43:56 -0500

    now serves single page applications

--- react_frontend/src/index.js
+++ react_frontend/src/index.js
@@ -1,13 +1,14 @@
 // @flow
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './pages/App';
 import { BrowserRouter } from 'react-router-dom';
+import App from './pages/App';
 
 ReactDOM.render(
     (
         <BrowserRouter>
             <App />
         </BrowserRouter>
-    ), document.getElementById('app'));
+    ), document.getElementById('app')
+);
 
