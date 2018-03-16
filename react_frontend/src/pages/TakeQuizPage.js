833b4d388783985c2d5f95641d2898688b482605
emileenoh <emileenoh@gmail.com>
2018-03-15 22:19:23 -0500

    it's a quiz component now

--- react_frontend/src/pages/TakeQuizPage.js
+++ react_frontend/src/pages/TakeQuizPage.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
-import Question from '../components/question'
-import Answer from '../components/answer'
+import Quiz from '../components/quiz';
 
 class TakeQuizPage extends Component{
     constructor(props){
@@ -14,26 +13,7 @@ class TakeQuizPage extends Component{
             //TODO: replace with some sort of query rendered for lesson-specific quiz?
             <div>
             <h1>Quiz 1</h1>
-                <div>
-                Question 1: ???
-                <Answer letter="A" />
-                <Answer letter="B" />
-                <Answer letter="C" />
-                <Answer letter="D" />
-                <br/>
-                Question 2: ???
-                <Answer letter="A" />
-                <Answer letter="B" />
-                <Answer letter="C" />
-                <Answer letter="D" />
-                <br/>
-                Question 3: ???
-                <Answer letter="A" />
-                <Answer letter="B" />
-                <Answer letter="C" />
-                <Answer letter="D" />
-                <br/>
-                </div>
+            <Quiz/>
             <button onClick={this.finish}>Finish</button>
             </div>
         );
