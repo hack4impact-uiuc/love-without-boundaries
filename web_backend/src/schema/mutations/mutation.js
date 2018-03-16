61ca0736fe7925e047ee019350e0d54414666cd2
Alan Ren <alanren2@illinois.edu>
2018-03-16 18:48:16 -0500

    finished restructuring mutations and using globalID's. past quizzes should work as well.

--- web_backend/src/schema/mutations/mutation.js
+++ web_backend/src/schema/mutations/mutation.js
@@ -111,15 +111,114 @@ const deleteAdmin = mutationWithClientMutationId({
       type: new GraphQLNonNull(GraphQLID),
     },
   },
-  // outputFields: {
-  //   admin: {
-  //     type: AdminType,
-  //     resolve: payload => payload,
-  //   },
-  // },
   mutateAndGetPayload: ({ id }) => {
     const obj = fromGlobalId(id)
     return Admin.findByIdAndRemove(obj.id);
+  },
+});
+
+const addGrade = mutationWithClientMutationId({
+  name: 'AddGrade',
+  inputFields: {
+    id: { type: new GraphQLNonNull(GraphQLID) },
+    lesson: { type: GraphQLString }, 
+    score: { type: GraphQLInt }
+  },
+  mutateAndGetPayload: ({ id, lesson, score }) => {
+    var grade = {"lesson": lesson, "score": score};
+    return Student.findOneAndUpdate({"_id": id}, {$push: {"grades": grade}})
+  },
+});
+
+const assignStudentToTeacher = mutationWithClientMutationId({
+  name: 'AssignStudentToTeacher',
+  inputFields: {
+    studentID: {type: GraphQLString}, 
+    teacherID: {type: GraphQLString}
+  },
+  mutateAndGetPayload: ({ studentID, teacherID }) => {
+    if (Student.findById(studentID) && Teacher.findById(teacherID)) {
+      return Student.findByIdAndUpdate(studentID, {$set: { teacherID: teacherID }}) && Teacher.findByIdAndUpdate(teacherID, {$push: {"listOfStudentIDs": studentID}})
+    }
+  },
+});
+
+const submitQuiz = mutationWithClientMutationId({
+  name: 'SubmitQuiz',
+  inputFields: {
+    id: {type: GraphQLString},
+    pastQuiz: {type: InputPastQuizType},
+  },
+  mutateAndGetPayload: ({ id, pastQuiz }) => {
+    return Student.findByIdAndUpdate(id, {$push: {"pastQuizzes": pastQuiz}})
+  },
+});
+
+const addQuestion = mutationWithClientMutationId({
+  name: 'AddQuestion',
+  inputFields: {
+    id: { type: GraphQLString }, 
+    question: { type: InputQuestionType }
+  },
+  mutateAndGetPayload: ({ id, question }) => {
+    return Lesson.findOneAndUpdate({"_id": id}, {$push: {"questions": question}})
+  },
+});
+
+const deleteLesson = mutationWithClientMutationId({
+  name: 'DeleteLesson',
+  inputFields: {
+    id: {type: new GraphQLNonNull(GraphQLString)}
+  },
+  mutateAndGetPayload: ({ id }) => {
+    const obj = fromGlobalId(id)
+    return Lesson.findByIdAndRemove(obj.id);
+  },
+});
+
+const addNote = mutationWithClientMutationId({
+  name: 'AddNote',
+  inputFields: {
+    id: {type: GraphQLString},
+    name: {type: GraphQLString},
+    url: {type: GraphQLString}  
+  },
+  mutateAndGetPayload: ({ id, name, url }) => {
+    return Lesson.findByIdAndUpdate(id, {$set: {"notesName" : name, "notesURL" : url}})
+  },
+});
+
+const deleteNote = mutationWithClientMutationId({
+  name: 'DeleteNote',
+  inputFields: {
+    id: {type: GraphQLString},
+  },
+  mutateAndGetPayload: ({ id }) => {
+    const obj = fromGlobalId(id)
+    return Lesson.findByIdAndUpdate(obj.id, {$set: {"notesName" : null, "notesURL" : null}})
+  },
+});
+
+const addWorksheet = mutationWithClientMutationId({
+  name: 'AddWorksheet',
+  inputFields: {
+    id: {type: GraphQLString},
+    name: {type: GraphQLString},
+    url: {type: GraphQLString} 
+  },
+  mutateAndGetPayload: ({ id, name, url }) => {
+    return Lesson.findByIdAndUpdate(id, {$set: {"worksheetName": name, "worksheetURL": url}})
+  },
+});
+
+const deleteWorksheet = mutationWithClientMutationId({
+  name: 'DeleteWorksheet',
+  inputFields: {
+    id: {type: GraphQLString},
+  },
+  mutateAndGetPayload: ({ id }) => {
+    const obj = fromGlobalId(id)
+    return Lesson.findByIdAndUpdate(obj.id, {$set: {"worksheetName" : null, "worksheetURL" : null}})
   },
 });
 
@@ -131,147 +230,17 @@ const Mutation = new GraphQLObjectType({
       createStudent,
       createTeacher, 
       createAdmin,
-      //TODO: Everything beyond this point doesnt work yet
-      // createQuiz: {
-      //   type: QuizType,
-      //   args: { name: { type: GraphQLString },
-      //           questions: { type: new GraphQLList(InputQuestionType) }
-      //         },
-      //   resolve(root, { name, questions }, ctx) {
-      //     const q = new Quiz({ name, questions });
-      //     return q.save()
-      //   } 
-      // },
       deleteAdmin, 
-      // deleteAdmin: { 
-      //   type: AdminType,
-      //   args: { id: {type: new GraphQLNonNull(GraphQLID)} },
-      //   resolve(root, {id}, ctx){
-      //     return Admin.findByIdAndRemove(id);
-      //   }
-      // },
-      // deleteQuiz: { 
-      //   type: QuizType,
-      //   args: { id: {type: new GraphQLNonNull(GraphQLID)} },
-      //   resolve(root, {id}, ctx){
-      //     return Quiz.findByIdAndRemove(id);
-      //   }
-      // },
-      addGrade: {
-        type: StudentType,
-        args: { id: { type: GraphQLString }, lesson: { type: GraphQLString }, score: { type: GraphQLInt } },
-        resolve(root, { id, lesson, score}, ctx) {
-          var grade = {"lesson": lesson, "score": score};
-          return Student.findOneAndUpdate({"_id": id}, {$push: {"grades": grade}})
-        } 
-      }, 
-      assignStudentToTeacher: {
-        type: StudentType, 
-        args: {studentID: {type: GraphQLString}, teacherID: {type: GraphQLString}}, 
-        resolve(root, {studentID, teacherID}, ctx) {
-        if (Student.findById(studentID) && Teacher.findById(teacherID)) {
-          return Student.findByIdAndUpdate(studentID, {$set: { teacherID: teacherID }}) && Teacher.findByIdAndUpdate(teacherID, {$push: {"listOfStudentIDs": studentID}})
-        }
-        }
-      },
-      submitQuiz: {
-        type: StudentType,
-        args: {
-          id: {type: GraphQLString},
-          pastQuiz: {type: InputPastQuizType},
-        },
-        resolve(root, { id, pastQuiz }, ctx) {
-          return Student.findByIdAndUpdate(id, {$push: {"pastQuizzes": pastQuiz}})
-        }
-      },
-      addQuestion: {
-        type: LessonType,
-        args: { id: { type: GraphQLString }, question: { type: InputQuestionType } },
-        resolve(root, { id, question }, ctx) {
-          return Lesson.findOneAndUpdate({"_id": id}, {$push: {"questions": question}})
-        } 
-      }, 
-      // addQuestion: {
-      //   type: QuizType,
-      //   args: { id: { type: GraphQLString }, question: { type: InputQuestionType } },
-      //   resolve(root, { id, question }, ctx) {
-      //     return Quiz.findOneAndUpdate({"_id": id}, {$push: {"questions": question}})
-      //   } 
-      // }, 
-      // deleteQuestion: {
-      //   type: QuestionType,
-      //   args: { id: { type: GraphQLString }, qID: { type: GraphQLString } },
-      //   resolve(root, { id, qID }, ctx) {
-      //     return Question.findByIdAndRemove(qID)
-      //     // return Quiz.findById(id, Question.findByIdAndRemove(qID))
-      //     // return Quiz.findByIdAndRemove(id)
-      //     // return Quiz.findOneAndUpdate({"_id": id}, {$pullAll: {"questions": [Quiz.find({questionName: qName})]}})
-      //   } 
-      // }, 
+      addGrade,
+      assignStudentToTeacher,
+      submitQuiz,
+      addQuestion, 
       createLesson,
-      // createLesson: {
-      //   type: LessonType,
-      //   args: {
-      //     name: {type: GraphQLString }, 
-      //     quiz: { type: InputQuizType }, 
-      //     worksheetName: { type: GraphQLString}, 
-      //     worksheetURL: { type: GraphQLString},
-      //     notesName: {type: GraphQLString},
-      //     notesURL: {type: GraphQLString},
-      //   },
-      //   resolve(root, { name, quiz, worksheetName, worksheetURL, notesName, notesURL}, ctx) {
-      //     const s = new Lesson({name, quiz, worksheetName, worksheetURL, notesName, notesURL})
-      //     return s.save()
-      //   }
-      // },
-      deleteLesson: { 
-        type: LessonType,
-        args: { id: {type: new GraphQLNonNull(GraphQLString)} },
-        resolve(root, {id}, ctx){
-          return Lesson.findByIdAndRemove(id);
-        }
-      },
-      addNote: {
-        type: LessonType,
-        args: {
-          id: {type: GraphQLString},
-          name: {type: GraphQLString},
-          url: {type: GraphQLString}  
-        },
-        resolve(root, { id, name, url}, ctx) {
-          return Lesson.findByIdAndUpdate(id, {$set: {"notesName" : name, "notesURL" : url}})
-        }
-      },
-      deleteNote: {
-        type: LessonType,
-        args: {
-          id: {type: GraphQLString},
-        },
-        resolve(root, {id}) {
-          return Lesson.findByIdAndUpdate(id, {$set: {"notesName" : null, "notesURL" : null}})
-        }
-      },
-      addWorksheet: {
-        type: LessonType,
-        args: {
-          id: {type: GraphQLString},
-          name: {type: GraphQLString},
-          url: {type: GraphQLString} 
-        },
-        resolve(root, { id, name, url}, ctx) {
-          return Lesson.findByIdAndUpdate(id, {$set: {"worksheetName": name, "worksheetURL": url}})
-        }
-      },
-      deleteWorksheet: {
-        type: LessonType,
-        args: {
-          id: {type: GraphQLString},
-        },
-        resolve(root, {id}) {
-          return Lesson.findByIdAndUpdate(id, {$set: {"worksheetName" : null, "worksheetURL" : null}})
-        }
-      },
-      
+      deleteLesson,
+      addNote,
+      deleteNote,
+      addWorksheet,
+      deleteWorksheet,
     };
   },
 });
