704b9f55629e854ee2b3c6b2d4ae104793a7a744
Alan Ren <alanren2@illinois.edu>
2018-03-15 23:27:22 -0500

    restructure mutations. did create ones have to finish the rest

--- web_backend/src/schema/mutations/mutation.js
+++ web_backend/src/schema/mutations/mutation.js
@@ -15,40 +15,148 @@ import {  TeacherType, AdminType, StudentType, LessonType, QuizType } from '../t
 import Question from '../types/QuestionType';
 import InputPastQuizType from '../types/InputPastQuizType';
 
+const createStudent = mutationWithClientMutationId({
+  name: 'CreateStudent',
+  inputFields: {
+    name: {
+      type: new GraphQLNonNull(GraphQLString),
+    },
+    email: {
+      type: new GraphQLNonNull(GraphQLString),
+    },
+  },
+  outputFields: {
+    student: {
+      type: StudentType,
+      resolve: payload => payload,
+    },
+  },
+  mutateAndGetPayload: ({ name, email }) => {
+    const s = new Student({ name, email });
+    return s.save();
+  },
+});
+
+const createTeacher = mutationWithClientMutationId({
+  name: 'CreateTeacher',
+  inputFields: {
+    name: {
+      type: new GraphQLNonNull(GraphQLString),
+    },
+    email: {
+      type: new GraphQLNonNull(GraphQLString),
+    },
+  },
+  outputFields: {
+    teacher: {
+      type: TeacherType,
+      resolve: payload => payload,
+    },
+  },
+  mutateAndGetPayload: ({ name, email }) => {
+    const t = new Teacher({ name, email });
+    return t.save();
+  },
+});
+
+const createAdmin = mutationWithClientMutationId({
+  name: 'CreateAdmin',
+  inputFields: {
+    name: {
+      type: new GraphQLNonNull(GraphQLString),
+    },
+    email: {
+      type: new GraphQLNonNull(GraphQLString),
+    },
+  },
+  outputFields: {
+    admin: {
+      type: AdminType,
+      resolve: payload => payload,
+    },
+  },
+  mutateAndGetPayload: ({ name, email }) => {
+    const a = new Admin({ name, email });
+    return a.save();
+  },
+});
+
+const createLesson = mutationWithClientMutationId({
+  name: 'CreateLesson',
+  inputFields: {
+    name: { type: new GraphQLNonNull(GraphQLString) },
+    quiz: { type: InputQuizType },
+    worksheetName: { type: GraphQLString },
+    worksheetURL: { type: GraphQLString },
+    notesName: { type: GraphQLString },
+    notesURL: { type: GraphQLString },
+  },
+  outputFields: {
+    admin: {
+      type: LessonType,
+      resolve: payload => payload,
+    },
+  },
+  mutateAndGetPayload: ({ name, quiz, worksheetName, worksheetURL, notesName, notesURL }) => {
+    const l = new Lesson({name, quiz, worksheetName, worksheetURL, notesName, notesURL})
+    return l.save()
+  },
+});
+
+const deleteAdmin = mutationWithClientMutationId({
+  name: 'DeleteAdmin',
+  inputFields: {
+    id: {
+      type: new GraphQLNonNull(GraphQLID),
+    },
+  },
+  // outputFields: {
+  //   admin: {
+  //     type: AdminType,
+  //     resolve: payload => payload,
+  //   },
+  // },
+  mutateAndGetPayload: ({ id }) => {
+    return Admin.findByIdAndRemove(id);
+  },
+});
 
 const Mutation = new GraphQLObjectType({
   name: 'Mutation',
   description: 'Your Root Mutation',
   fields() {
     return {
-      createStudent: {
-        type: StudentType,
-        args: { name: { type: GraphQLString }, email: { type: GraphQLString } },
-        resolve(root, { name, email}, ctx) {
-          const s = new Student({ name, email });
-          return s.save()
-        } 
-      },
-      createTeacher: {
-        type: TeacherType,
-        args: { name: { type: GraphQLString }, 
-                email: { type: GraphQLString }
-              },
-        resolve(root, { name, email }, ctx) {
-          const t = new Teacher({ name, email });
-          return t.save()
-        } 
-      },
-      createAdmin: {
-        type: AdminType,
-        args: { name: { type: GraphQLString },
-                email: { type: GraphQLString }
-              },
-        resolve(root, { name, email }, ctx) {
-          const a = new Admin({ name, email });
-          return a.save()
-        } 
-      },
+      createStudent,
+      // createStudent: {
+      //   type: StudentType,
+      //   args: { name: { type: GraphQLString }, email: { type: GraphQLString } },
+      //   resolve(root, { name, email}, ctx) {
+      //     const s = new Student({ name, email });
+      //     return s.save()
+      //   } 
+      // },
+      createTeacher, 
+      // createTeacher: {
+      //   type: TeacherType,
+      //   args: { name: { type: GraphQLString }, 
+      //           email: { type: GraphQLString }
+      //         },
+      //   resolve(root, { name, email }, ctx) {
+      //     const t = new Teacher({ name, email });
+      //     return t.save()
+      //   } 
+      // },
+      createAdmin,
+      // createAdmin: {
+      //   type: AdminType,
+      //   args: { name: { type: GraphQLString },
+      //           email: { type: GraphQLString }
+      //         },
+      //   resolve(root, { name, email }, ctx) {
+      //     const a = new Admin({ name, email });
+      //     return a.save()
+      //   } 
+      // },
       // createQuiz: {
       //   type: QuizType,
       //   args: { name: { type: GraphQLString },
@@ -59,13 +167,14 @@ const Mutation = new GraphQLObjectType({
       //     return q.save()
       //   } 
       // },
-      deleteAdmin: { 
-        type: AdminType,
-        args: { id: {type: new GraphQLNonNull(GraphQLID)} },
-        resolve(root, {id}, ctx){
-          return Admin.findByIdAndRemove(id);
-        }
-      },
+      deleteAdmin, 
+      // deleteAdmin: { 
+      //   type: AdminType,
+      //   args: { id: {type: new GraphQLNonNull(GraphQLID)} },
+      //   resolve(root, {id}, ctx){
+      //     return Admin.findByIdAndRemove(id);
+      //   }
+      // },
       // deleteQuiz: { 
       //   type: QuizType,
       //   args: { id: {type: new GraphQLNonNull(GraphQLID)} },
@@ -124,21 +233,22 @@ const Mutation = new GraphQLObjectType({
       //     // return Quiz.findOneAndUpdate({"_id": id}, {$pullAll: {"questions": [Quiz.find({questionName: qName})]}})
       //   } 
       // }, 
-      createLesson: {
-        type: LessonType,
-        args: {
-          name: {type: GraphQLString }, 
-          quiz: { type: InputQuizType }, 
-          worksheetName: { type: GraphQLString}, 
-          worksheetURL: { type: GraphQLString},
-          notesName: {type: GraphQLString},
-          notesURL: {type: GraphQLString},
-        },
-        resolve(root, { name, quiz, worksheetName, worksheetURL, notesName, notesURL}, ctx) {
-          const s = new Lesson({name, quiz, worksheetName, worksheetURL, notesName, notesURL})
-          return s.save()
-        }
-      },
+      createLesson,
+      // createLesson: {
+      //   type: LessonType,
+      //   args: {
+      //     name: {type: GraphQLString }, 
+      //     quiz: { type: InputQuizType }, 
+      //     worksheetName: { type: GraphQLString}, 
+      //     worksheetURL: { type: GraphQLString},
+      //     notesName: {type: GraphQLString},
+      //     notesURL: {type: GraphQLString},
+      //   },
+      //   resolve(root, { name, quiz, worksheetName, worksheetURL, notesName, notesURL}, ctx) {
+      //     const s = new Lesson({name, quiz, worksheetName, worksheetURL, notesName, notesURL})
+      //     return s.save()
+      //   }
+      // },
       deleteLesson: { 
         type: LessonType,
         args: { id: {type: new GraphQLNonNull(GraphQLString)} },
