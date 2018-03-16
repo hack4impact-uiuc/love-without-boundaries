6d44bcc7a12221bdc127952da5fdce34d14cbf44
Timothy Ko <timothy.l.ko@gmail.com>
2018-03-16 12:25:07 -0500

    added browser router

--- react_frontend/src/index.js
+++ react_frontend/src/index.js
@@ -2,7 +2,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './pages/App';
+import { BrowserRouter } from 'react-router-dom';
 
-
-ReactDOM.render(<App />, document.getElementById('app'),);
+ReactDOM.render(
+    (
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    ), document.getElementById('app'));
 
