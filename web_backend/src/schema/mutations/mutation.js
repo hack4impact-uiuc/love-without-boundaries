5e125aa4d22ad8189b64586ef4651816d41a1ac6
CodeBrew28 <arshiam2@illinois.edu>
2018-04-05 22:56:22 -0500

    almost done with submit quiz

--- web_backend/src/schema/mutations/mutation.js
+++ web_backend/src/schema/mutations/mutation.js
@@ -210,27 +210,76 @@ const submitQuiz = mutationWithClientMutationId({
     mutateAndGetPayload: async ({
         id, lessonID, questions, answers,
     }) => {
-        const sObj = fromGlobalId(id);
+        // const sObj = fromGlobalId(id);
         const lObj = fromGlobalId(lessonID);
         // console.log("hi");
         const q1 = await Lesson.findById(lObj.id).exec();
-        const quizName = q1.name;
-        let score = 0;
-        let submittedAnswers = [];
+        // const quizName = q1.name;
+        // let score = 0;
+        // let submittedAnswers = [];
 
         // console.log(questions[0]);
         // console.log(answers[0]);
         // console.log(q1.quiz.questions[0].answers)
         // // numberCorrect = 0;
-        const questionNames = q1.quiz.questions.map(q => q.questionName )
-        // console.log(questionNames)
+        const questionNames = q1.quiz.questions.map(q => q.questionName);
+        const answerNames = q1.quiz.questions.map(q => q.answers);
+
+        // console.log(questionNames);
+        // console.log(answerNames);
+        let numCorrect = 0;
+        questions.forEach((q, i) => {
+            let isCorrect = true;
+            const indexOfQuestion = questionNames.reduce((a, e, i) => { if (e === q) a.push(i); return a; }, []);
+            q1.quiz.questions[indexOfQuestion].answers.map(a => ((a.isCorrect) ? isCorrect = isCorrect && (a.answerName == answers[i]) : null));
+            numCorrect += isCorrect;
+        });
+
+        // pastQuizzes: [{
+        //     quizName: { type: String },
+        //     score: { type: Number },
+        //     submittedAnswers: [{
+        //         questionID: { type: String },
+        //         answerChosen: { type: String },
+        //         correctAnswer: { type: String },
+        //     }],
+        // }],
+
+        var selectedAnswers = []
+        q1.quiz.questions.forEach((q, i) => {
+            let isCorrect = true;
+            console.log(q.questionName)
+            // questionNames.findIndex(q.questionName)
+            // const answerChosen = questionNames.reduce((a, e, i) => { if (e === q) a.push(i); return a; }, []);
+            const indexOfAnswer = ( questions.findIndex(element => element === q.questionName) )
+            if (indexOfAnswer != -1) {
+                selectedAnswers.push(answers[indexOfAnswer])
+
+            } else {
+                selectedAnswers.push("No answer selected");
+            }
+            numCorrect += isCorrect;
+        });
+
+        console.log(selectedAnswers)
+        const pastQuizzes = {
+            quizName: q1.name,
+            score: (numCorrect / questionNames.length),
+            submittedAnswers: 'lol',
+        };
+        console.log(pastQuizzes);
+        // console.log(q1.name)
+
+        console.log(numCorrect / questionNames.length);
+
+
         // const quizAnswers = q1.quiz.questions.map(q => answers)
         // console.log(quizAnswers)
 
         // for (question in q1.quiz.questions){
         //     console.log(question);
         // }
-        
+
         // q1.quiz.questions.forEach(element => {
         //     console.log(element)
         // });
@@ -254,7 +303,7 @@ const submitQuiz = mutationWithClientMutationId({
         //     //     score += 1;
         //     // }
         //     // submittedAnswers.push({ qid, ans, correctA });
-            
+
         //     // submittedAnswers.push("hi");
         // }));
         // console.log('lol');
