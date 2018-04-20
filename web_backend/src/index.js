8842b679517309093d3dd4c9ac58d9f5c63690bd
CodeBrew28 <arshiam2@illinois.edu>
2018-04-19 23:03:04 -0500

    wip

--- web_backend/src/index.js
+++ web_backend/src/index.js
@@ -17,7 +17,8 @@ const MONGO_URI = 'mongodb://ariamalkani:malkani@ds147228.mlab.com:47228/lwb';
 import jwt from 'jsonwebtoken';
 import find from 'lodash/find';
 import { isNull } from 'util';
-const expiresIn = '3h'; 
+
+const expiresIn = '3h';
 const secret = 'samplejwtauthgraphql'; // secret key
 const tokenPrefix = 'JWT'; // Prefix for HTTP header
 
@@ -26,8 +27,10 @@ export const createToken = async (name, email, token, role) => {
     if (!name || !email || !token || !role) { // no credentials = fail
         return false;
     }
-    const user = await User.find({"name": name, "email": email, "token": token, "role":role })
-    if (user.length === 0) { 
+    const user = await User.find({
+        name, email, token, role,
+    });
+    if (user.length === 0) {
         return false;
     }
     const payload = {
@@ -44,12 +47,14 @@ export const createToken = async (name, email, token, role) => {
 
 
 export const verifyToken = async (payload) => {
-    if (!payload) { 
+    if (!payload) {
         throw new Error('No token provided');
     }
-    const data = (jwt.verify(payload, 'samplejwtauthgraphql'))
-    const user = await User.find({"name": data.name, "email": data.email, "token": data.token, "role":data.role })
-    if (user.length === 0) { 
+    const data = (jwt.verify(payload, 'samplejwtauthgraphql'));
+    const user = await User.find({
+        name: data.name, email: data.email, token: data.token, role: data.role,
+    });
+    if (user.length === 0) {
         return false;
     }
     return true;
@@ -65,25 +70,35 @@ mongoose.connection
 const withAuth = next => async (req, res) => {
     req.user = null;
     const header = req.headers.authorization;
-    // if (header) {
-    //     const [type, token] = header.split(' ');
-    //     switch (type) {
-    //     case 'Bearer':
-    //         // TODO: Find user in Teacher/Student/Admin model and set r
-    //         break;
-    //     }
-    //     console.log("yo")
-        
-    // }
-    // console.log¿(req)
-    //TODO: If it a register call, dont auth
-    const token = await createToken("aria", "aria@gmail.com", "123", "student");
-    if (await verifyToken(token)) {
-        return next(req, res);
+
+
+    if (header) {
+        const [type, token] = header.split(' ');
+        switch (type) {
+        case 'Bearer':
+            // TODO: Find user in Teacher/Student/Admin model and set r
+            // console.log(token);
+            if (await verifyToken(token)) {
+                console.log('verified');
+                return next(req, res);
+            }
+            break;
+        }
     }
+
+    // console.log('yo');
+
+    // // }
+    // console.log(header);
+    // // TODO: If it a register call, dont auth
+    // const token = await createToken('aria', 'aria@gmail.com', '123', 'student');
+    // console.log(token);
+    // if (await verifyToken(token)) {
+    //     return next(req, res);
+    // }
     return null;
 
     // return next(req, res);
 };
 
-exports.default = cors(withAuth(graphqlHTTP({ schema: Schema, pretty: true, graphiql: true } ) ) );
+exports.default = cors(withAuth(graphqlHTTP({ schema: Schema, pretty: true, graphiql: true })));
