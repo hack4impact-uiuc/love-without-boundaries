6f6de1cc19f99c7b390bad245578a6a1f89064a1
Timothy Ko <timothy.l.ko@gmail.com>
2018-04-20 05:00:43 -0500

    fxied student lessons

--- react_frontend/src/components/studentLesson.js
+++ react_frontend/src/components/studentLesson.js
@@ -10,32 +10,50 @@ class StudentLesson extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            worksheetIDs: [],
-            lessonIDs: [],
+            error: '',
         };
     }
 
     componentDidMount() {
+        if (this.props.studentWorksheets === null) {
+            this.setState({
+                error: 'No lessons exist, please contact admin',
+            });
+            return;
+        }
         const studentWorksheetLessonIDs = this.props.studentWorksheets.worksheets.map(element => element.lessonID);
         if (studentWorksheetLessonIDs == undefined) {
-            alert('No lessons exist, please contact admin');
+            this.setState({
+                error: 'No lessons exist, please contact admin',
+            });
+            return;
         }
         let i;
+        const error = false;
         for (i = 0; i < this.props.lessons.length; i++) {
             if (!(studentWorksheetLessonIDs.includes(this.props.lessons[i].id))) {
                 addStudentWorksheetCopy(environment, this.props.location.state.student.id, this.props.lessons[i].id, this.props.lessons[i].worksheetURL);
                 const url = this.props.lessons[i].worksheetURL;
-                const fileId = url.match(/[-\w]{25,}/)[0];
+                console.log('url', url);
+                const fileMatch = url.match(/[-\w]{25,}/);
+                if (fileMatch === null || fileMatch === undefined) {
+                    return;
+                }
+                const fileId = fileMatch[0];
                 copyFile(fileId).then((res) => {
                     if (res == undefined || res.error) {
-                        throw Error('Insufficient Priviledges, please contact Admin');
+                        return;
+                        // throw Error('Insufficient Priviledges, please contact Admin');
                     }
                     setPermissionToAllEdit(res.id);
-                }).catch(err => alert(err.message));
+                }).catch(err => console.err(err.message));
             }
         }
     }
     render() {
+        if (this.state.error !== '') {
+            return <p>{this.state.error}</p>;
+        }
         return (
             <div className="container-fluid">
                 <h2>
@@ -46,7 +64,19 @@ class StudentLesson extends React.Component {
                 <GoogleDocButton url="https://docs.google.com/document/d/1EGbrZFxY33xyEZdLyXmKGdWi5NR4CL7nS4C_7HzhSgE/edit" />
                 {
                     this.props.lessons.map((lesson, idx) => (
-                        <LessonComponent key={idx}id={lesson.id} lessonName={lesson.name} lessonNotes={lesson.notesName} lessonNotesLink={lesson.notesURL} lessonWorksheetLink={lesson.worksheetURL} worksheetName={lesson.worksheetName} quizName={lesson.quiz} quizPercentage="50%" quizIsChecked={false} isTeacher={this.state.isTeacher} />
+                        <LessonComponent
+                            key={idx}
+                            id={lesson.id}
+                            lessonName={lesson.name}
+                            lessonNotes={lesson.notesName}
+                            lessonNotesLink={lesson.notesURL}
+                            lessonWorksheetLink={lesson.worksheetURL}
+                            worksheetName={lesson.worksheetName}
+                            quizName={lesson.quiz}
+                            quizPercentage="50%"
+                            quizIsChecked={false}
+                            isTeacher={this.state.isTeacher}
+                        />
                     ))
                 }
             </div>
