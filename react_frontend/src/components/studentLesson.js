e3a3726bc78ba1d7d0eb971c9472dc13d0f6f018
shreyshrey1 <talk2shreyas@gmail.com>
2018-04-26 00:24:51 -0500

    comments and logs deleted

--- react_frontend/src/components/studentLesson.js
+++ react_frontend/src/components/studentLesson.js
@@ -16,62 +16,31 @@ class StudentLesson extends React.Component {
 
     componentDidMount() {
         const studentWorksheetLessonIDs = this.props.studentWorksheets.worksheets.map(element => element.lessonID);
-        console.log('hi');
         let i;
-        const error = false;
-        console.log(this.props.lessons.length);
         const indices = [];
         const promises = [];
         for (i = 0; i < this.props.lessons.length; i++) {
             if (!(studentWorksheetLessonIDs.includes(this.props.lessons[i].id))) {
                 const url = this.props.lessons[i].worksheetURL;
-                console.log('url', url);
                 const fileMatch = url.match(/[-\w]{25,}/);
                 if (fileMatch === null || fileMatch === undefined) {
                     return;
                 }
                 const fileId = fileMatch[0];
-                console.log(fileId);
                 promises.push(copyFile(fileId));
                 indices.push(i);
             }
         }
         Promise.all(promises).then((res) => {
-            console.log(res);
-            console.log(indices);
             for (i = 0; i < res.length; i++) {
                 if (res[i] == undefined || res.error) {
                     throw Error('Insufficient Privilges, please contact Admin');
                 }
-                console.log('Adding wksht to db');
                 setPermissionToAllEdit(res[i].id);
                 addStudentWorksheetCopy(environment, this.props.location.state.student.id, this.props.lessons[indices[i]].id, `https://docs.google.com/document/d/${res[i].id}/edit`);
             }
         }).catch(err => console.error(err.message));
     }
-    // for (i = 0; i < this.props.lessons.length; i += 1) {
-    //     if (!(studentWorksheetLessonIDs.includes(this.props.lessons[i].id))) {
-    //         const url = this.props.lessons[i].worksheetURL;
-    //         console.log('url', url);
-    //         const fileMatch = url.match(/[-\w]{25,}/);
-    //         if (fileMatch === null || fileMatch === undefined) {
-    //             return;
-    //         }
-    //         const fileId = fileMatch[0];
-    //         copyFile(fileId).then((res) => {
-    //             console.log('i', i);
-    //             if (res == undefined || res.error) {
-    //                 // return;
-    //                 throw Error('Insufficient Priviledges, please contact Admin');
-    //             }
-    //             setPermissionToAllEdit(res.id);
-    //             console.log(this.props.location.state.student);
-    //             console.log(this.props.lessons);
-    //             console.log(i);
-    //             addStudentWorksheetCopy(environment, this.props.location.state.student.id, this.props.lessons[i].id, `https://docs.google.com/document/d/${res.id}/edit`);
-    //         }).catch(err => console.error(err.message));
-    //     }
-    // }
     render() {
         if (this.state.error !== '') {
             return <p>{this.state.error}</p>;
